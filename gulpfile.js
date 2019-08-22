6e9791b9974750c76aaf28f838be637b1a6e39f1
Anna <pogorelova_ann@mail.ru>
2019-08-23 01:05:09 +0500

    Добавлен хедер

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var minify = require('gulp-csso');
+var concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('style', function () {
     return gulp.src('src/sass/style.scss')
@@ -16,3 +18,22 @@ gulp.task('style', function () {
         ]))
         .pipe(gulp.dest('src/css'));
 });
+
+gulp.task('scripts', function() {
+    return gulp.src([
+            'src/js/libs/stickyfill.min.js',
+            'src/js/scripts.js'
+        ])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('src/js'));
+});
+
+gulp.task('images', function() {
+    return gulp.src('src/img/**/*.{png,jpg}')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest('docs/img'));
+});
+
