b5f53d7b6e5ec23d86c4013e9d6e2de5be0be9ff
Anna <pogorelova_ann@mail.ru>
2019-08-22 15:56:04 +0500

    Создан проект

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var minify = require('gulp-csso');
+
+gulp.task('style', function () {
+    return gulp.src('src/sass/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest('src/css'));
+});
